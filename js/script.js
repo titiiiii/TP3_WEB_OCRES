1b22782e9956495e7b41dc9bbaf00899c6abe410
Thibault Trebuchon <thibault.trebuchon@edu.ece.fr>
2021-09-28 12:25:26 +0200

    Q1,2,3

--- js/script.js
+++ js/script.js
@@ -29,3 +29,31 @@ function start() {
       console.error(error);
     });
 }
+
+function reload(){
+  const apiWeather = new API_WEATHER(document.getElementById("city-input").value);
+  
+  apiWeather
+    .fetchTodayForecast()
+    .then(function(response) {
+      // Récupère la donnée d'une API
+      const data = response.data;
+
+      // On récupère l'information principal
+      const main = data.weather[0].main;
+      const description = data.weather[0].description;
+      const temp = data.main.temp;
+      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+
+      // Modifier le DOM
+      document.getElementById('today-forecast-main').innerHTML = main;
+      document.getElementById('today-forecast-more-info').innerHTML = description;
+      document.getElementById('icon-weather-container').innerHTML = icon;
+      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      
+    })
+    .catch(function(error) {
+      // Affiche une erreur
+      console.error(error);
+    });
+}
